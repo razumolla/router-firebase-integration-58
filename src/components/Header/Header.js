2c6d9ae8696d6db90e52602677ff0a6cbf228b10
razumolla <rajuahmed9344@gmail.com>
2022-04-12 21:08:05 +0600

    googleProvider add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
+
 const Header = () => {
+    const { user } = useFirebase()
     return (
         <div className='header'>
             <nav>
@@ -9,7 +12,10 @@ const Header = () => {
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/register">Register</Link>
-                <Link to="/login">Login</Link>
+                {user.uid ?
+                    <button>Sign out</button> :
+                    <Link to="/login">Login</Link>
+                }
             </nav>
         </div>
     );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../firebase.init'
+
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setUser] = useState({})
 
-    useEffect(() => {
+    // useEffect(() => {
+    // }, [])
 
-    }, [])
     const singInWithGoogle = () => {
-        // signInWithPopup(auth, googleProvider)
-        //     .then(result => {
-        //         const user = result.user;
-        //         setUser(user);
-        console.log(user);
-        // })
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
     }
-    return [user, setUser]
+    return { user, singInWithGoogle }
 }
 export default useFirebase;
