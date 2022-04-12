5f4c558ffaf3dcc6112241b64b4bcc4f0c8b5259
razumolla <rajuahmed9344@gmail.com>
2022-04-12 23:08:25 +0600

    requireauth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -11,8 +14,15 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        {/* <Route path="/orders" element={<Orders />}></Route> */}
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders> </Orders>
+          </RequireAuth>
+        }> </Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
 import './Header.css'
 
+const auth = getAuth(app)
 const Header = () => {
-    const { user, handleSignOut } = useFirebase()
+    const [user, loading, error] = useAuthState(auth);
     return (
         <div className='header'>
             <nav>
@@ -15,7 +18,7 @@ const Header = () => {
                 <span>{user?.displayName && user.displayName}</span>
                 {user?.uid
                     ?
-                    <button onClick={handleSignOut}>Sign out</button> :
+                    <button onClick={() => signOut(auth)}>Sign out</button> :
                     <Link to="/login">Login</Link>
                 }
             </nav>
